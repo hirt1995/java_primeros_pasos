105ad554165c19ce6ca3fcbdba73988f57707fc0
Hugo Isaac Rodríguez <hirt1995@gmail.com>
2023-03-25 00:46:32 -0500

    Update TestReferencia.java

--- src/TestReferencia.java
+++ src/TestReferencia.java
@@ -2,7 +2,7 @@ public class TestReferencia {
     public static void main(String[] args) {
         Cliente hugo = new Cliente();
         hugo.nombre = "Hugo";
-        hugo.documento = "1116800954";
+        hugo.documento = "45342234";
         hugo.telefono = "8856158";
 
         cuenta cuentadeHugo = new cuenta();
